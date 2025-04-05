3bfc4c463137fea7065fd4c4f67c915068b8bc38
shengistired <35317976+shengistired@users.noreply.github.com>
2025-04-05 19:37:51 +0800

    initial commit

---
+++ src/main/java/com/crypto/trading/system/cryptotradingsystem/CryptotradingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.trading.system.cryptotradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotradingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotradingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/trading/system/cryptotradingsystem/CryptotradingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.trading.system.cryptotradingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotradingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
